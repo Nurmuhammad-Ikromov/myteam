a329e1174e358ad5dfcab58a9e5f19fd7dfa3ff2
Nurmuhammad-Ikromov <Nurmuhammadikromov58@gmail.com>
2022-05-16 15:39:09 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ elButtonX.addEventListener("click", function(){
     elMenu.classList.remove("navbar--open");
     elButtonX.classList.remove("burger-button-x--open");
 })
+
+var elBtn = document.querySelector(".directors__button");
+var elContent = document.querySelector(".directors__item-hidden");
+var elBox = document.querySelector(".directors__item-box");
+
+elBtn.addEventListener("click", function(){
+    elContent.classList.toggle("directors__item--open");
+    elBox.classList.toggle(".directors__item-box--open")
+})
